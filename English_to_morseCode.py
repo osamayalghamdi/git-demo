947dc1a0822ebc2aef14fcfffd2f8f0f33ccc489
osama alghamdi <osamayalghamdi@gmail.com>
2024-04-22 21:37:41 +0300

    first commit

---
+++ English_to_morseCode.py
@@ -0,0 +1,29 @@
+#This project just for fun :)
+
+english_to_morse = {
+    # Alphabets
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 
+    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 
+    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 
+    'Z': '--..',
+    
+    #space
+    ' ': '/',
+
+    # Numbers
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', 
+    '4': '....-', '5': '.....', '6': '-....', '7': '--...', 
+    '8': '---..', '9': '----.',
+}
+user_input = input("enter your text: ")
+def text(text):
+    result = ""
+    text = text.upper()
+    for i in text:
+        if (i in english_to_morse):
+            result+= english_to_morse[i]+" "
+
+    return result
+print('this is your text: '+ user_input + "\nand this is the morse code: "+text(user_input))
---
+++ README.txt
@@ -0,0 +1,1 @@
+Hello This project just for fun
